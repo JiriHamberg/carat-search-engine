2a9937e63c270fca627980b62ff3cf9f771798c7
Jiri Hamberg <jiri.hamberg@gmail.com>
2017-05-18 12:50:51 +0300

    Did not work. Moved back to jvm1.7

--- project/build.scala
+++ project/build.scala
@@ -22,7 +22,8 @@ object CarapAPIProtoBuild extends Build {
       version := Version,
       scalaVersion := ScalaVersion,
       //force old jvm because users.cs.helsinki.fi uses tomcat6
-      scalacOptions += "-target:jvm-1.6",
+      //scalacOptions += "-target:jvm-1.6",
+      //javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
       resolvers += Classpaths.typesafeReleases,
       resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
       libraryDependencies ++= Seq(
