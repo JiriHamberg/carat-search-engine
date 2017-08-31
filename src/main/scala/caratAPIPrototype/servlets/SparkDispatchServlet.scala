78b66b671432039a613023d84a3f7dbc0a7a7b4e
Jiri Hamberg <jiri.hamberg@gmail.com>
2017-08-31 20:53:04 +0300

    Fix: changed Seq to List in case class that is parsed by json4s - to properly map to json array

--- src/main/scala/caratAPIPrototype/servlets/SparkDispatchServlet.scala
+++ src/main/scala/caratAPIPrototype/servlets/SparkDispatchServlet.scala
@@ -17,14 +17,10 @@ import org.json4s.JsonAST.{JValue}
 import org.scalatra.json._
 import org.json4s.JsonDSL._
 
-//import org.json4s.{DefaultFormats, Formats}
-//import org.scalatra.json._
-
-
 case class SparkJobOptions(
   minSupport: Option[Double],
   minConfidence: Option[Double],
-  excluded: Seq[String]
+  excluded: List[String]
 )
 
 class SparkDispatchServlet extends ScalatraServlet with JacksonJsonSupport with FutureSupport {
@@ -32,14 +28,12 @@ class SparkDispatchServlet extends ScalatraServlet with JacksonJsonSupport with 
 	implicit val executor =  ExecutionContext.global
   val conf = ConfigFactory.load()
 	override val asyncTimeout = conf.getInt("spark-server.timeout") seconds
-  //protected implicit lazy val jsonFormats: Formats = DefaultFormats
 
   before() {
     contentType = formats("json")
   }
 
 	post("/") {
-      //contentType = formats("json")
       val sparkJobOptions = parsedBody.extract[SparkJobOptions]
 	    Future(SparkDispatcher.postRequest(sparkJobOptions))
   	}
