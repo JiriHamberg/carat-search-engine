6fe44bdf63f13b134b6a61ef94c88e49037267d2
Jiri Hamberg <jiri.hamberg@gmail.com>
2017-11-29 19:02:06 +0200

    Fix: AppCounts

--- src/main/scala/caratAPIPrototype/services/AppCounts.scala
+++ src/main/scala/caratAPIPrototype/services/AppCounts.scala
@@ -4,14 +4,14 @@ import scalaj.http.{Http, HttpOptions}
 import com.typesafe.config._
 import scala.concurrent.duration._
 
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
+//import org.json4s._
+//import org.json4s.jackson.JsonMethods._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 
 object AppCounts {
-	implicit val formats = org.json4s.DefaultFormats
+	//implicit val formats = org.json4s.DefaultFormats
   implicit val executor = ExecutionContext.global
 
   val conf = ConfigFactory.load()
